7d66a05ed9f7329120da03f4421d74a5279c842c
Divyansh3021 <divyansh3021@gmail.com>
2023-05-21 19:13:40 +0530

    just a try

--- api/text_summarization.py
+++ api/text_summarization.py
@@ -30,5 +30,5 @@ def summarize():
         return text_summarize(text)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000)
